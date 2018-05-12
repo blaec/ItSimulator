c18c37e821c8111840304f6a6b68064a754ce28f
blaec <odeskonst@yandex.ru>
2018-05-12 12:07:47 +0300

    021_071 testing cache with Mockito

--- germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/service/transform/impl/CachedFieldProviderTest.java
@@ -1,31 +1,41 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
+import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Verifies functionality of the {@link SimpleDTOTransformer}
- * unit
+ * Verifies functionality of the {@link SimpleDTOTransformer} unit
+ * 
  * @author Morenets
  *
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ReflectionUtil.class)
 public class CachedFieldProviderTest {
 	private FieldProvider provider;
-	
+
 	@Before
 	public void setup() {
 		provider = new CachedFieldProvider();
 	}
-	
+
 	@Test
 	public void testGetFieldNamesSuccess() {
 		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
 		assertFalse(fields.isEmpty());
-		assertTrue(fields.contains("value"));		
+		assertTrue(fields.contains("value"));
 	}
 
 	@Test
@@ -33,9 +43,22 @@ public class CachedFieldProviderTest {
 		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
 		List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
 		assertFalse(fields.isEmpty());
-		assertEquals(fields, fields2);		
+		assertEquals(fields, fields2);
 	}
-	
+
+	@Test
+	public void testGetFieldNamesAreCached() {
+		List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+		PowerMockito.mockStatic(ReflectionUtil.class);
+		when(ReflectionUtil.findSimilarFields(Source.class, Destination.class)).thenReturn(Collections.emptyList());
+
+		assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
+		List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+		assertFalse(fields.isEmpty());
+		assertEquals(fields, fields2);
+	}
+
 }
 
 class Source {
